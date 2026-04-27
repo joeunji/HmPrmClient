5c17e98769ba0b9554313bffc2d6624f66d9c814
yomile <yomile@msn.com>
2026-04-27 14:26:27 +0900

    푸시적용

--- ios/HmPrmClient/AppDelegate.swift
+++ ios/HmPrmClient/AppDelegate.swift
@@ -1,48 +1,51 @@
 import UIKit
 import React
+import Firebase
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-  var window: UIWindow?
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
+	var window: UIWindow?
 
-  var reactNativeDelegate: ReactNativeDelegate?
-  var reactNativeFactory: RCTReactNativeFactory?
+	var reactNativeDelegate: ReactNativeDelegate?
+	var reactNativeFactory: RCTReactNativeFactory?
 
-  func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
-  ) -> Bool {
-    let delegate = ReactNativeDelegate()
-    let factory = RCTReactNativeFactory(delegate: delegate)
-    delegate.dependencyProvider = RCTAppDependencyProvider()
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool
+	{
+		// Firebase 초기화
+		if FirebaseApp.app() == nil
+		{
+			FirebaseApp.configure()
+		}
 
-    reactNativeDelegate = delegate
-    reactNativeFactory = factory
+		let delegate = ReactNativeDelegate()
+		let factory = RCTReactNativeFactory(delegate: delegate)
+		delegate.dependencyProvider = RCTAppDependencyProvider()
 
-    window = UIWindow(frame: UIScreen.main.bounds)
+		reactNativeDelegate = delegate
+		reactNativeFactory = factory
 
-    factory.startReactNative(
-      withModuleName: "HmPrmClient",
-      in: window,
-      launchOptions: launchOptions
-    )
+		window = UIWindow(frame: UIScreen.main.bounds)
 
-    return true
-  }
+		factory.startReactNative(withModuleName: "HmPrmClient",	in: window,	launchOptions: launchOptions)
+
+		return true
+	}
 }
 
 class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
-  override func sourceURL(for bridge: RCTBridge) -> URL? {
-    self.bundleURL()
-  }
+	override func sourceURL(for bridge: RCTBridge) -> URL?
+	{
+		self.bundleURL()
+	}
 
-  override func bundleURL() -> URL? {
+	override func bundleURL() -> URL? {
 #if DEBUG
-    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+		RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
 #else
-    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+	Bundle.main.url(forResource: "main", withExtension: "jsbundle")
 #endif
-  }
+	}
 }
